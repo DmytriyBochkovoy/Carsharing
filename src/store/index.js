76d1eac682775c884eeedb08f1fe0843ef31c174
Dmytriy <bochkovoi.dmytriy@gmail.com>
2022-01-30 23:23:33 +0200

    New logic and components

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import  { createStore } from 'vuex';
 import cars from './cars';
+import testimonials from './testimonials'
 
 export default createStore ({
   namespaced: true,
 
   modules: {
     cars,
+    testimonials,
   },
 });
